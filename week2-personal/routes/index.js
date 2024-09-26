17ade1c490540307175e51fca82c1cd9df0e6ce7
Sarah Glenn <sarahadaline@gmail.com>
2024-09-26 13:20:32 +0200

    uggh moving folders and fixing stuff

--- week2-personal/routes/index.js
+++ week2-personal/routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.send('Welcome to the Contacts API! -Sarah Glenn')
+})
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
